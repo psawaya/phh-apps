312f236a7a025a9176dc63d7aded08e760ece13f
Paul Mason <paul@paulmason.me>
2022-07-12 11:38:11 -0700

    Test harness basics for apps

---
+++ src/Harness.svelte
@@ -0,0 +1,177 @@
+<script>
+  import _ from "lodash";
+  import { PLAYER_NAMES } from "./seed";
+
+  let loaded = false;
+  let allPlayers = [];
+  let originalUrl = 'https://apps.elsewhere.zone/?appName=blackjack';
+  let appUrl = '';
+  let appId = '123';
+  let appData = {};
+  let baseDomain = '';
+
+  const configureApp = () => {
+    const parsed = new URL(originalUrl);
+    baseDomain = `${parsed.protocol}//${parsed.host}`;
+    appUrl = originalUrl;
+    if (!parsed.searchParams.get('appId')) {
+      addUrlParam('appId', appId);
+    } else {
+      appId = parsed.searchParams.get('appId');
+    }
+    if (!parsed.searchParams.get('appHost')) {
+      const harnessLocation = new URL(window.location);
+      const appHost = `${harnessLocation.protocol}//${harnessLocation.host}`;
+      console.log(`appHost: ${appHost}`);
+      addUrlParam('appHost', appHost, false);
+    }
+    if (!parsed.searchParams.get('debug')) {
+      console.log('debug enabled');
+      addUrlParam('debug', 'true');
+    }
+
+    appData = {};
+    window.addEventListener('message', appListener);
+
+    loaded = true;
+  };
+
+  const simulatePlayerJoin = () => {
+    const player = { id: new Date().getTime(), name: _.sample(PLAYER_NAMES) };
+
+    // Let Svelte know this has been updated
+    allPlayers = [...allPlayers, player];
+  };
+
+  const refreshAllFrames = () => {
+    console.log('refreshAllFrames', allPlayers);
+  };
+
+  const reset = () => {
+    allPlayers = [];
+    appData = {};
+    appUrl = '';
+    baseDomain = '';
+    window.removeEventListener('message', appListener);
+    loaded = false;
+  };
+
+  const addUrlParam = (key, value, encode = true) => {
+    if (appUrl.indexOf('?') < 0) {
+      appUrl += '?';
+    } else {
+      appUrl += '&';
+    }
+    if (encode) {
+      appUrl += `${key}=${encodeURIComponent(value)}`;
+    } else {
+      appUrl += `${key}=${value}`;
+    }
+  };
+
+  const appStart = (id) => {
+    appPostMessage(id, {
+      type: 'load',
+      data: appData,
+    });
+  };
+  const appPlayerLeave = (id) => {
+    // TODO: Send a message and remove the player
+  };
+  const appPostMessage = (id, data) => {
+    const iFrame = document.getElementById(`app${id}`);
+    if (iFrame) {
+      console.log('posting data to url', baseDomain);
+      iFrame.contentWindow.postMessage(data, baseDomain);
+    }
+  };
+  const appListener = (msg) => {
+    if (msg.origin !== baseDomain) {
+      return;
+    }
+    // TODO: Consider case of msg.data is not valid JSON
+    const msgData = JSON.parse(msg.data);
+    // Ignore message intended for other appId
+    if (msgData.appId !== appId.toString()) {
+      return;
+    }
+
+    const source = Array.from(document.getElementsByTagName('iframe')).filter(iframe => {
+      return iframe.contentWindow === msg.source;
+    })[0];
+    const localPlayerId = source.id.replace('app', '') * 1;
+
+    switch (msgData.data.type) {
+      case 'update':
+        const { key, value } = msgData.data.data;
+        if (key) {
+          appData[key] = value;
+          // Also, post a change event to each of the players
+          for (const player of allPlayers) {
+            appPostMessage(player.id, { type: 'change', data: appData });
+          }
+        }
+        break;
+      case 'get_players':
+        const playersData = allPlayers.map((player) => {
+          return {
+            id: player.id,
+            sessionId: player.id,
+            name: player.name,
+            localPlayer: localPlayerId === player.id,
+          };
+        });
+        appPostMessage(localPlayerId, {
+          type: 'get_players',
+          data: playersData,
+        });
+        break;
+      case 'announce':
+      case 'set_frame_height':
+        // Unsupported.
+        // Formerly, Elsewhere v1 allowed apps to announce a message via snackbar to an entire space.
+        // Or directly set their iframe height in pixels.
+        break;
+      default:
+        console.log(`unhandled message: ${msgData.data.type}`);
+        break;
+    }
+  };
+</script>
+<style>
+.row {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: flex-start;
+
+  color: #333;
+  gap: 10px;
+}
+h3 {
+  color: #333;
+}
+</style>
+{#if !loaded}
+<div class="row">
+  <div style="vertical-align: middle; line-height: 32px;">ApplicationURL:</div>
+  <input type="text" placeholder="Application URL" style="width: 500px" bind:value={originalUrl} />
+  <button on:click={configureApp}>Configure</button>
+</div>
+{/if}
+{#if loaded}
+<div class="row">
+  <button on:click={simulatePlayerJoin}>Simulate Player Joining</button>
+  <button on:click={refreshAllFrames}>Refresh all Frames</button>
+  <button on:click={reset}>Reset</button>
+</div>
+<div>
+  {#each allPlayers as player}
+    <div class="frame">
+      <h3>{player.name} <button on:click={appPlayerLeave.bind(this, player.id)}>Leave</button></h3>
+      <iframe src="{appUrl}" style="width: 100%;" on:load={appStart.bind(this, player.id)} id="app{player.id}">
+
+      </iframe>
+    </div>
+  {/each}
+</div>
+{/if}
